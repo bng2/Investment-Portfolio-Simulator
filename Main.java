27a86d64023436c0e759d269fa694bb5c15fb9f1
bng2 <46977566+bng2@users.noreply.github.com>
2021-05-03 19:14:36 -0400

    Delete Main.java
    relocated

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    User test = new User("John", 12.00);
-    System.out.println(test.viewBalance());
-    test.addBalance(13);
-    System.out.println(test.viewBalance());
-  }
-}
